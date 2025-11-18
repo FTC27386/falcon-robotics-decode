dc4ae5876391f761244c278ba741bd4e237a8bac
Tsumihara <155585876+Tsumihara@users.noreply.github.com>
2025-11-18 11:56:37 -0500

    Update InterpLUTAim.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/InterpLUTAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/InterpLUTAim.java
@@ -162,6 +162,7 @@ public class InterpLUTAim extends OpMode {
         distanceX = targetX - pinpointX;
         distanceY = targetY - pinpointY;
         //turret_angle = gamepad1.right_trigger;
+        field_relative_angle = Math.toRadians(-90)-Math.atan2(distanceX, distanceY);
         turret_angle = 1-(-localizer.getHeading(AngleUnit.DEGREES)+313)/626;
 
         //distanceVector = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
